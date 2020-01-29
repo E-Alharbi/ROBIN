14d9e64b85ce47b864f62ef1f99c4a7f705b80e4
Emad Alharbi <emra500@york.ac.uk>
2020-01-29 15:39:28 +0000

    read from txt file to string

--- src/PMBPP/Utilities/TxtFiles.java
+++ src/PMBPP/Utilities/TxtFiles.java
@@ -1,6 +1,9 @@
 package PMBPP.Utilities;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class TxtFiles {
@@ -10,4 +13,17 @@ public class TxtFiles {
 		    out.println(Txt );
 		}
 	}
+	public String readFileAsString(String filePath) throws IOException {
+        StringBuffer fileData = new StringBuffer();
+        BufferedReader reader = new BufferedReader(
+                new FileReader(filePath));
+        char[] buf = new char[1024];
+        int numRead=0;
+        while((numRead=reader.read(buf)) != -1){
+            String readData = String.valueOf(buf, 0, numRead);
+            fileData.append(readData);
+        }
+        reader.close();
+        return fileData.toString();
+    }
 }
