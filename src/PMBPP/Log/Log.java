f299f1b512cc76fd0f99eeac87d44409890290f6
Emad Alharbi <emra500@york.ac.uk>
2021-04-24 05:27:43 +0100

    Output the predictions in a HTML table with a bar plot

--- src/PMBPP/Log/Log.java
+++ src/PMBPP/Log/Log.java
@@ -57,7 +57,7 @@ public class Log {
 		return FlipTable.of(headers, data);
 
 	}
-public String CreateHTMLTable(List<String> headersList, List<List<String>> rowsList) {
+public String HTMLTable(List<String> headersList, List<List<String>> rowsList) {
 	String Table="<tr>";
 	for(int i=0 ; i < headersList.size() ; ++i) {
 		Table+="<th>"+headersList.get(i)+"</th>";
@@ -72,6 +72,74 @@ public String CreateHTMLTable(List<String> headersList, List<List<String>> rowsL
 	}
 	
 	return Table;
+}
+public String HTMLPlot(List<String> headersList, List<List<String>> rowsList) {
+	
+	String Table="<table id=\"datatable\">\n"
+			+ "	<thead> <tr>";
+	for(int i=0 ; i < headersList.size() ; ++i) {
+		if(i==0)
+		Table+="<th>"+headersList.get(i)+"</th>";
+		else
+			Table+="<th>"+headersList.get(i)+"(%)</th>";
+	}
+	Table+="</tr> </thead> <tbody>";
+	for(int i=0 ; i < rowsList.size(); ++i) {
+		Table+="<tr>";
+		for(int r=0 ; r < rowsList.get(i).size();++r) {
+			if(r!=0 && !rowsList.get(i).get(r).trim().equals("-")) {
+				
+				String Val=rowsList.get(i).get(r).trim();
+				if(headersList.get(r).trim().equals("R-free") || headersList.get(r).trim().equals("R-work")) {
+					Val=Val.substring(2);
+					
+			}
+				Table+="<td>"+Val+"</td>";
+			}
+			else {
+			Table+="<td>"+rowsList.get(i).get(r)+"</td>";
+			}
+		}
+		Table+="</tr>";
+	}
+	Table+="</tbody> </table>";
+	return Table;
+	
+	
+	/*
+	String Table="";
+	for(int i=1 ; i < headersList.size() ; ++i) {
+		 Table+="<table class=\"graph\">\n"
+				+ "	<caption> "+headersList.get(i)+" </caption>\n"
+				+ "	<thead>";
+		Table+="<th scope=\"col\">"+headersList.get(0)+"</th>";
+		Table+="<th scope=\"col\"> "+headersList.get(i)+"</th> </tr>\n"
+				+ "	</thead> <tbody>";
+	
+		
+		//System.out.println(rowsList.size());
+		for(int pipe=0 ; pipe < rowsList.size(); ++pipe)
+		{	//System.out.println(rowsList.get(pipe).size());
+			//System.out.println(rowsList.get(pipe).get(0));
+			//System.out.println(rowsList.get(pipe).get(1));
+			//System.out.println(rowsList.get(pipe).get(2));
+			if(!rowsList.get(pipe).get(i).trim().equals("-")) {
+			double Val=Double.valueOf(rowsList.get(pipe).get(i).trim());
+			if(headersList.get(i).trim().equals("R-free") || headersList.get(i).trim().equals("R-work")) {
+				Val=Val*100.0;
+		}
+			Table+="<tr style=\"height:"+Val+"%\">";
+			Table+="<th scope=\"row\">"+rowsList.get(pipe).get(0)+"</th>";
+			Table+="<td><span>"+Val+"%</span></td>";
+			Table+="</tr>";
+			}
+		}
+		
+		Table+="</tbody>\n"
+				+ "</table> \n";
+	}
+	return Table;
+	*/
 }
 	public void Error(Object obj, String msg) {
 		if (Parameters.getLog().equals("T"))
