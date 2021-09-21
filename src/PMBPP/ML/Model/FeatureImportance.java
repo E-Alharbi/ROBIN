e45a571c4ae8369a194a085658426ebba5388769
Emad Alharbi <emra500@york.ac.uk>
2021-09-21 04:42:20 +0100

    Ablation studies

---
+++ src/PMBPP/ML/Model/FeatureImportance.java
@@ -0,0 +1,21 @@
+package PMBPP.ML.Model;
+
+import java.util.HashMap;
+
+public class FeatureImportance extends HashMap<String,MLEvaluation> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String HashMapName;
+
+	public String getHashMapName() {
+		return HashMapName;
+	}
+
+	public void setHashMapName(String hashMapName) {
+		HashMapName = hashMapName;
+	}
+	
+}
