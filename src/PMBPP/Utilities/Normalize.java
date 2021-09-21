1dd7b28b54a425176d803c8df7160eed62fdccf2
Emad Alharbi <emra500@york.ac.uk>
2021-09-21 04:48:06 +0100

    Normalize/Scaling to scale 0-1

---
+++ src/PMBPP/Utilities/Normalize.java
@@ -0,0 +1,23 @@
+package PMBPP.Utilities;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import PMBPP.ML.Model.Parameters;
+
+public class Normalize {
+
+	public String NormalizeVal(String Val , String StructureEvaluation, boolean round ) {
+		if(Parameters.getNormalizeStructureEvaluationInErrorTable().equals("T")) {
+			if(Parameters.getStructureEvaluationToBeNormalized().contains(StructureEvaluation)) {
+				if(round==true)
+				Val= new BigDecimal(Val).divide(new BigDecimal("100")).setScale(2,  RoundingMode.HALF_UP).toString();
+				else
+					Val= new BigDecimal(Val).divide(new BigDecimal("100")).toString();
+	
+			}
+		}
+		return Val;
+	}
+	
+}
